bd73d107a7d692387f0590ebea9f4d2a10c93ecd
Marjory Marquez <samaterra@hotmail.com>
2018-05-21 19:50:53 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,9 +3,45 @@
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
-        x.className += " responsive";
+        x.className += "responsive";
     } else {
         x.className = "topnav";
     }
 } 
 //END Section TopNav//
+
+//Section Home Background//
+//section data background//
+$(document).ready(function(){
+    $('section[data-type="background"]').each(function(){
+        var $bgobj = $(this); // assigning the object  
+        $(window).scroll(function() {
+            var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+ 
+// Put final background position
+      var coords = '50% '+ yPos + 'px';
+
+// Move the background
+        $bgobj.css({ backgroundPosition: coords });
+        }); 
+    });    
+});
+$(document).ready(function($) {
+
+	$('.card__share > a').on('click', function(e){ 
+		e.preventDefault() // prevent default action - hash doesn't appear in url
+   		$(this).parent().find( 'div' ).toggleClass( 'card__social--active' );
+		$(this).toggleClass('share-expanded');
+    });
+  
+});
+
+//End Section Background//
+//END Section Home Background//
+
+//API TEST//
+
+//END API TEST//
+//ANIMATION COL//
+
+//END ANIMATION COL//
